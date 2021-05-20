df9f046e3ecb08a62a882243d21eeed65b01b6d9
michaelab24 <michaelbarnes459@yahoo.com>
2021-05-20 14:36:37 -0500

    more cosmetic work and consol log click events

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ $(document).ready(function(){
     $("#currentDay").append(displayDate)
 })
 
+$("#save").on("click", function(event){
+    event.preventDefault();
+
+    console.log(event)
+});
+
 //add event input
 //save event input to local storage
